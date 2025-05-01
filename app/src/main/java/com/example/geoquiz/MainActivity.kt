4408580b1d90efd8c36c90100f7c8737f5a76c22
Jezaile <whocaresmymail@gmail.com>
2025-05-01 13:32:22 +0700

    Добавлена функция UpdateQuestion, выводящаяя вопрос из банка вопросов

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.geoquiz
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,24 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private  lateinit var nextButton: Button
+    private  val questionBank = listOf(
+        Question(R.string.question_australia,true),
+        Question(R.string.question_oceans,true),
+        Question(R.string.question_mideast,false),
+        Question(R.string.question_africa,false),
+        Question(R.string.question_americas,true),
+        Question(R.string.question_asia,true),
+        Question(R.string.question_mexico,false),
+        Question(R.string.question_zealand,true),
+        Question(R.string.question_china,true),
+        Question(R.string.question_paris,true),
+        Question(R.string.question_africa_land,true)
+    )
+    private var currentIndex = 0
+    private lateinit var questionTextView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,6 +42,8 @@ class MainActivity : AppCompatActivity() {
         }
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener{view: View ->
             Toast.makeText(
@@ -36,5 +57,11 @@ class MainActivity : AppCompatActivity() {
                 R.string.false_toast,
                 Toast.LENGTH_SHORT).show()
         }
+        UpdateQuestion()
+    }
+
+    fun UpdateQuestion(){
+        val questionTextResId = questionBank[currentIndex].TextResId
+        questionTextView.setText(questionTextResId)
     }
 }
